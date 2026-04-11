dff3b52814b41c45be98218e3a679142d97d2d0a
Solomon <yk2772@nyu.edu>
2026-04-11 15:00:37 +0200

    Initial Commit

---
+++ favoritePics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  favoritePics
+//
+//  Created by Solomon Kim on 4/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ favoritePics/favoritePicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  favoritePicsApp.swift
+//  favoritePics
+//
+//  Created by Solomon Kim on 4/11/26.
+//
+
+import SwiftUI
+
+@main
+struct favoritePicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
